7bd9ac9c8b6cf4aee6085b313c799a992f2e0fe2
김기섭 <kiseop91@naver.com>
2018-12-03 03:10:48 +0900

    이것도 합쳐줘...

--- MyFirstHelloWorld/소스.cpp
+++ MyFirstHelloWorld/소스.cpp
@@ -7,6 +7,14 @@ int main()
 	std::cout << "Hellow, world" << "Hellow, My friend" << "Hellow, god" << std::endl;
 	
 	cout << " using namespace std !!!!" << endl;
+
+	cout << " 깃이란 무엇이고, 풀리퀘스트란 무엇인가..?" << endl;
+
+	cout << " 원하는 오픈소스를 Fork를 눌러 내 저장소로 복사한다." << endl;
+
+	cout << " 그리고 자유롭게 수정하고 테스트한후에 원래 저장소에 내소스를 넣고싶다면..?" << endl;
+
+	cout << " 마스터브랜치를 가지고있는 사람에게 풀리퀘스트를 보내는것이다..!" << endll;
 	
 	return 0;
 }
