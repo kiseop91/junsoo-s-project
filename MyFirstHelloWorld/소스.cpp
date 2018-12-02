1bd6627105b88afaeded766078dc629dde859705
김기섭 <kiseop91@naver.com>
2018-12-02 23:33:32 +0900

    유징네임스페이스를 쓰면 편하다!!

--- MyFirstHelloWorld/소스.cpp
+++ MyFirstHelloWorld/소스.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
+using namespace std;
 
 int main()
 
 {
 	std::cout << "Hellow, world" << "Hellow, My friend" << "Hellow, god" << std::endl;
+	
+	cout << " using namespace std !!!!" << endl;
 	
 	return 0;
 }
